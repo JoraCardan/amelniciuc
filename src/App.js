fdd92d80ff879cc53c3e1e56d7f0aaa34a898560
Constantin <constantin@boltmobility.com>
2018-05-13 14:17:29 +0200

    Configured Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Portfolio from './pages/Portfolio';
+import Home from './pages/Home';
+import About from './pages/About';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <div className="content">
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/portfolio" component={Portfolio} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Project from './Project';
+
+const Portfolio = ({ match }) => {
+  return (
+    <React.Fragment>
+      <Route path={`${match.url}/:projectId`} component={Project} />
+      <Route
+        exact
+        path={match.url}
+        render={() => <h3>Please select a Project.</h3>}
+      />
+    </React.Fragment>
+  );
+};
+
+export default Portfolio;
